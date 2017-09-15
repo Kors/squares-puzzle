4d426b2b6402417e4debbf341755785027a22f2c
Kors <i.korsikov@spb.ant-inform.ru>
2017-09-15 12:57:22 +0300

    Refactoring. Extracted compare methods

--- src/main/scala/puzzle/PuzzleExecutor.scala
+++ src/main/scala/puzzle/PuzzleExecutor.scala
@@ -28,45 +28,73 @@ object PuzzleExecutor {
   def filterGroups(groups: ArrayBuffer[List[Square]]): ArrayBuffer[List[Square]] = {
     val upEquality = groups.filter(g => {
       var flag = false
-      for (other <- groups.filter(o => o != g)) {
-        if (g.head == other(2) && g(1) == other(3))
-          if (!other.contains(g(2)) &&
-            !other.contains(g(3)))
-            flag = true
-      }
+      for (other <- groups.filter(o => o != g))
+        if (matchesUpperSide(g, other))
+          flag = true
       flag
     })
     val downEquality = upEquality.filter(g => {
       var flag = false
-      for (other <- groups.filter(o => o != g)) {
-        if (g(2) == other.head && g(3) == other(1))
-          if (!other.contains(g.head) &&
-            !other.contains(g(1)))
-            flag = true
-      }
+      for (other <- groups.filter(o => o != g))
+        if (matchesDownSide(g, other))
+          flag = true
       flag
     })
     val leftEquality = downEquality.filter(g => {
       var flag = false
-      for (other <- groups.filter(o => o != g)) {
-        if (g.head == other(1) && g(2) == other(3))
-          if (!other.contains(g(1)) &&
-            !other.contains(g(3)))
-            flag = true
-      }
+      for (other <- groups.filter(o => o != g))
+        if (matchesLeftSide(g, other))
+          flag = true
       flag
     })
     val rightEquality = leftEquality.filter(g => {
       var flag = false
-      for (other <- groups.filter(o => o != g)) {
-        if (g(1) == other.head && g(3) == other(2))
-          if (!other.contains(g.head) &&
-            !other.contains(g(2)))
-            flag = true
-      }
+      for (other <- groups.filter(o => o != g))
+        if (matchesRightSide(g, other))
+          flag = true
       flag
     })
     rightEquality
+  }
+
+  def matchesUpperSide(centralGroup: List[Square], anotherGroup: List[Square]): Boolean = {
+    if (centralGroup.head == anotherGroup(2) &&
+      centralGroup(1) == anotherGroup(3) &&
+      !anotherGroup.contains(centralGroup(2)) &&
+      !anotherGroup.contains(centralGroup(3)))
+      true
+    else
+      false
+  }
+
+  def matchesDownSide(centralGroup: List[Square], anotherGroup: List[Square]): Boolean = {
+    if (centralGroup(2) == anotherGroup.head &&
+      centralGroup(3) == anotherGroup(1) &&
+      !anotherGroup.contains(centralGroup.head) &&
+      !anotherGroup.contains(centralGroup(1)))
+      true
+    else
+      false
+  }
+
+  def matchesLeftSide(centralGroup: List[Square], anotherGroup: List[Square]): Boolean = {
+    if (centralGroup.head == anotherGroup(1) &&
+      centralGroup(2) == anotherGroup(3) &&
+      !anotherGroup.contains(centralGroup(1)) &&
+      !anotherGroup.contains(centralGroup(3)))
+      true
+    else
+      false
+  }
+
+  def matchesRightSide(centralGroup: List[Square], anotherGroup: List[Square]): Boolean = {
+    if (centralGroup(1) == anotherGroup.head &&
+      centralGroup(3) == anotherGroup(2) &&
+      !anotherGroup.contains(centralGroup.head) &&
+      !anotherGroup.contains(centralGroup(2)))
+      true
+    else
+      false
   }
 
 }
