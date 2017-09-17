b86ccc022bfc3afc6fd34b1a7ff0bd1f4eab57d5
Ilia Korsikov <ilia_korsikov@epam.com>
2017-09-17 17:41:34 +0300

    Refactoring remove unnecessary methods

--- src/main/scala/puzzle/PuzzleExecutor.scala
+++ src/main/scala/puzzle/PuzzleExecutor.scala
@@ -7,12 +7,12 @@ import scala.collection.mutable.ArrayBuffer
 object PuzzleExecutor {
 
   def execute(squares: List[Square]): List[FullPuzzle] = {
-    val pairs = getAllMatchedPairs(squares)
-    val groupsOfFour = PuzzleExecutor.getAllMatchedQuarters(pairs)
-    val groupsOfSix = getAllMatchedSixs(groupsOfFour)
-    val groupsOfEight = getAllMatchedEights(groupsOfSix, groupsOfFour)
-    val groupsOfTen = getAllMatchedTens(groupsOfEight, groupsOfFour)
-    PuzzleExecutor.getResults(groupsOfTen, groupsOfFour)
+    val pairs = getValues(squares, squares, PairOfSquares.apply)
+    val groupsOfFour = getValues(pairs, pairs, SquareOfSquares.apply)
+    val groupsOfSix = getValues(groupsOfFour, groupsOfFour, SixOfSquares.apply)
+    val groupsOfEight = getValues(groupsOfSix, groupsOfFour, EightOfSquares.apply)
+    val groupsOfTen = getValues(groupsOfTen, groupsOfFour, TenOfSquares.apply)
+    getValues(groupsOfTen, groupsOfFour, FullPuzzle.apply)
   }
 
   def getValues[T, R, S](firstList: List[T], secondList: List[R], function2: Function2[T, R, Option[S]]): List[S] = {
@@ -24,23 +24,5 @@ object PuzzleExecutor {
     )
     buf.toList
   }
-
-  def getAllMatchedPairs(squares: List[Square]): List[PairOfSquares] =
-    getValues(squares, squares, PairOfSquares.apply)
-
-  def getAllMatchedQuarters(pairs: List[PairOfSquares]): List[SquareOfSquares] =
-    getValues(pairs, pairs, SquareOfSquares.apply)
-
-  def getAllMatchedSixs(sqOfSq: List[SquareOfSquares]): List[SixOfSquares] =
-    getValues(sqOfSq, sqOfSq, SixOfSquares.apply)
-
-  def getAllMatchedEights(sixOfSq: List[SixOfSquares], fourOfSq: List[SquareOfSquares]): List[EightOfSquares] =
-    getValues(sixOfSq, fourOfSq, EightOfSquares.apply)
-
-  def getAllMatchedTens(eightOfSq: List[EightOfSquares], fourOfSq: List[SquareOfSquares]): List[TenOfSquares] =
-    getValues(eightOfSq, fourOfSq, TenOfSquares.apply)
-
-  def getResults(tenOfSq: List[TenOfSquares], fourOfSq: List[SquareOfSquares]): List[FullPuzzle] =
-    getValues(tenOfSq, fourOfSq, FullPuzzle.apply)
 
 }
