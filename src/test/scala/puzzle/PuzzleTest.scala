80ea0526e34b16e7263643dad78ee3feb4ba1be7
Ilia Korsikov <ilia_korsikov@epam.com>
2017-09-19 23:01:41 +0300

    Add writing test

--- src/test/scala/puzzle/PuzzleTest.scala
+++ src/test/scala/puzzle/PuzzleTest.scala
@@ -1,36 +1,43 @@
 package puzzle
 
 import org.scalatest.FunSpec
-import puzzle.PuzzleExecutor.getValues
-import puzzle.objects.{EightOfSquares, PairOfSquares, SixOfSquares, SquareOfSquares}
+import puzzle.objects.Square
 
 import scala.io.Source
 
 class PuzzleTest extends FunSpec {
   info("Starting...")
 
+  describe("IO tests") {
+    it("read from file successfully") {
+      val squares = DataIO.read(inputFileName)
+      println("squares size:" + squares.size)
+      assert(squares.size == 12, squares)
+    }
+
+    it("write to file successfully") {
+      val testFile = "testFile"
+      DataIO.write(Seq(Square("1 2 3 4").toFormattedString), testFile)
+      val actualResults = read(testFile)
+      val expectedResults = List("1 2 3 4", "")
+      assertResult(expectedResults)(actualResults)
+    }
+  }
+
   val inputFileName = "src/main/resources/example4.txt"
   val outputFileName = "src/main/resources/test_output.txt"
   val resultToCompare = "src/main/resources/output4.txt"
 
-  describe("Read") {
-    it("read from file successfully") {
+  describe("Integration tests") {
+    it("full cycle") {
       val squares = DataIO.read(inputFileName)
-      assert(squares.size == 12, squares)
-    }
-  }
-
-  describe("Group") {
-    it("final results") {
-      val squares = DataIO.read(inputFileName)
-      println("squares size:" + squares.size)
       val results = PuzzleExecutor.execute(squares)
       println("results size:" + results.size)
       DataIO.write(results, outputFileName)
       assert(results.nonEmpty)
-      val output = read(outputFileName)
-      val correct = read(resultToCompare)
-      assert(output == correct)
+      val actualResults = read(outputFileName)
+      val expectedResults = read(resultToCompare)
+      assertResult(expectedResults)(actualResults)
     }
   }
 
