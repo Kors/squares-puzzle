64b7075a338e274cf7422bbf9e3ccace88574320
Ilia Korsikov <ilia_korsikov@epam.com>
2017-09-19 22:41:22 +0300

    Refactoring. Change style

--- src/main/scala/puzzle/PuzzleExecutor.scala
+++ src/main/scala/puzzle/PuzzleExecutor.scala
@@ -6,22 +6,16 @@ object PuzzleExecutor {
 
   def execute(squares: Seq[Square]): List[FullPuzzle] = {
     val pairs = getValues(squares, squares, PairOfSquares.apply)
-    println("pairs size:" + pairs.size)
     val groupsOfFour = getValues(pairs, pairs, SquareOfSquares.apply)
-    println("groups of 4 size:" + groupsOfFour.size)
     val groupsOfSix = getValues(groupsOfFour, groupsOfFour, SixOfSquares.apply)
-    println("groups of 6 size:" + groupsOfSix.size)
     val groupsOfEight = getValues(groupsOfSix, groupsOfFour, EightOfSquares.apply)
-    println("groups of 8 size:" + groupsOfEight.size)
     val groupsOfTen = getValues(groupsOfEight, groupsOfFour, TenOfSquares.apply)
-    println("groups of 10 size:" + groupsOfTen.size)
     getValues(groupsOfTen, groupsOfFour, FullPuzzle.apply)
   }
 
-  def getValues[T, R, S](firstList: Seq[T], secondList: Seq[R], function2: Function2[T, R, Option[S]]): List[S] = {
-    firstList.par.flatMap(list1 =>
-      secondList.filter(list => list != list1).map(list2 => function2(list1, list2))
-    ).filter(o => o.isDefined).map(o => o.get).toList
-  }
+  def getValues[T, R, S](firstSeq: Seq[T], secondSeq: Seq[R], factory: (T, R) => Option[S]): List[S] =
+    firstSeq.par
+      .flatMap(seq1 => secondSeq.filter(seq => seq != seq1).map(seq => factory(seq1, seq)))
+      .filter(o => o.isDefined).map(o => o.get).toList
 
 }
