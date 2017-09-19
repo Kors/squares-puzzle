2e5a636d02e9b00f1f552c5be9fa23201b18d38c
Ilia Korsikov <ilia_korsikov@epam.com>
2017-09-19 23:23:35 +0300

    Add Square factory tests

--- src/test/scala/puzzle/PuzzleTest.scala
+++ src/test/scala/puzzle/PuzzleTest.scala
@@ -12,7 +12,7 @@ class PuzzleTest extends FunSpec {
     it("read from file successfully") {
       val squares = DataIO.read(inputFileName)
       println("squares size:" + squares.size)
-      assert(squares.size == 12, squares)
+      assertResult(12)(squares.size)
     }
 
     it("write to file successfully") {
@@ -21,6 +21,24 @@ class PuzzleTest extends FunSpec {
       val actualResults = read(testFile)
       val expectedResults = List("1 2 3 4", "")
       assertResult(expectedResults)(actualResults)
+    }
+  }
+
+  describe("Factory tests") {
+    it("create square successfully") {
+      val square = Square("1 2 3 4")
+      assertResult(1)(square.leftUp)
+      assertResult(2)(square.rightUp)
+      assertResult(3)(square.leftDown)
+      assertResult(4)(square.rightDown)
+    }
+
+    it("fail to create square (wrong argument numbers)") {
+      assertThrows[IllegalArgumentException](Square("12 3 4"))
+    }
+
+    it("fail to create square (wrong int value)") {
+      assertThrows[IllegalArgumentException](Square("1 2.34 3 4"))
     }
   }
 
