437125a27d45ba26b2dcaa6b9e799b03f12ff4ee
Ilia Korsikov <ilia_korsikov@epam.com>
2017-09-20 10:39:31 +0300

    Add seqMatchRules method

--- src/main/scala/puzzle/objects/CompoundFigure.scala
+++ src/main/scala/puzzle/objects/CompoundFigure.scala
@@ -12,35 +12,26 @@ trait CompoundFigure {
   // те условия, которые предполагаются проверенными ранее, не учитываются.
   // (да, для понимания сложно, но проще это написать не выходит т.к. очень разные условия на каждом шаге)
 
-  protected def isCorrect(seq: Seq[Square]): Boolean = {
-
-    seq.size == figureSize &&
-      hasNoDuplicates(seq) && {
-      seq.size match {
-        case (2) =>
-          seq.head.rightUp + seq(1).leftUp <= maxSum && seq.head.rightDown + seq(1).leftDown <= maxSum
-
-        case (4) =>
-          seq.head.rightDown + seq(1).leftDown + seq(2).rightUp + seq(3).leftUp == maxSum &&
-            seq.head.leftDown + seq(2).leftUp <= maxSum &&
-            seq(1).rightDown + seq(3).rightUp <= maxSum
-
-        case (6) => true
-        case (8) => true
-
-        case (10) =>
-          seq(1).rightDown + seq(3).rightUp + seq(4).leftUp <= maxSum &&
-            seq(6).rightDown + seq(7).leftDown + seq(9).rightUp <= maxSum
-
-        case (12) =>
-          seq.head.leftDown + seq(2).rightUp + seq(3).leftUp <= maxSum &&
-            seq(6).rightDown + seq(7).leftDown + seq(10).leftUp <= maxSum
-
-        case _ => false
-      }
+  private def seqMatchRules(seq: Seq[Square]): Boolean = {
+    seq.size match {
+      case (2) => seq.head.rightUp + seq(1).leftUp <= maxSum && seq.head.rightDown + seq(1).leftDown <= maxSum
+      case (4) => seq.head.rightDown + seq(1).leftDown + seq(2).rightUp + seq(3).leftUp == maxSum &&
+        seq.head.leftDown + seq(2).leftUp <= maxSum &&
+        seq(1).rightDown + seq(3).rightUp <= maxSum
+      case (6) => true
+      case (8) => true
+      case (10) => seq(1).rightDown + seq(3).rightUp + seq(4).leftUp <= maxSum &&
+        seq(6).rightDown + seq(7).leftDown + seq(9).rightUp <= maxSum
+      case (12) => seq.head.leftDown + seq(2).rightUp + seq(3).leftUp <= maxSum &&
+        seq(6).rightDown + seq(7).leftDown + seq(10).leftUp <= maxSum
+      case _ => false
     }
-
   }
+
+  protected def isCorrect(seq: Seq[Square]): Boolean =
+    seq.size == figureSize &&
+      hasNoDuplicates(seq) &&
+      seqMatchRules(seq)
 
 }
 
